e3b0c5bdd471906a14c347e8e7c2e47d0b11d721
尹龙云 <lone_yin@163.com>
2020-01-17 11:55:13 +0800

    update version 1.0.9

--- deploy.config.js
+++ deploy.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  projectName: "fast-deploy-tool", // 项目名称
+  projectName: "", // 项目名称
   prod: {
     host: "118.31.19.191", // 主机名 必填
     username: "root", // ssh用户名 必填
@@ -7,8 +7,8 @@ module.exports = {
     password: "DFnshngdgwmsx1", // ssh密码  可选  和私钥路径二选一
     privateKey: '/Users/lone/.ssh/id_rsa', // rsa私钥路径  可选  和ssh密码二选一
     dirctories: ["/dist"], // 要上传的文件夹目录 数组 可选
-    files: ["/dist.zip"], // 要上传的文件 数组 可选
-    commandsBeforeUpload: [], // 连接服务器后，在上传之前需要执行的命令行
-    commandsAfterUpload: [] // 连接服务器后，在上传之后需要执行的命令行
+    files: [], // 要上传的文件 数组 可选
+    cmdBeforeUpload: [], // 连接服务器后，在上传之前需要执行的命令行
+    cmdAfterUpload: [] // 连接服务器后，在上传之后需要执行的命令行
   }
 };
